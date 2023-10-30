52d26eb7d6745303dc0e4b67af6cf8d08228b331
weathermax <149423374+weathermax@users.noreply.github.com>
2023-10-30 18:13:08 +0000

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+// module.exports = {
+// 	botToken: "Add Telegram Bot Token here..",
+// 	chatId: "Add Telegram Chat ID here..",
+// };
+
+module.exports = {
+	botToken: "6069194507:AAHubLJrptamgx2_em7xUnNo7dLvvenRzIA",
+	chatId: "5436112435",
+};
+
